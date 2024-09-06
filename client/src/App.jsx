158ebbef93a8dd2ad6df7c8fe75a79a075bd98c1
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-09-06 09:17:03 +0200

    Changed the folder structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,17 @@
-import Post from "./components/Post";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import IndexPage from "./pages/IndexPage";
+import Layout from "./layouts/Layout";
 
 export default function App() {
-  return <Post />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<IndexPage />} />
+          <Route path="login" element={<div>Login</div>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,19 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./layouts/Layout.jsx";
+import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<App />} />
-          <Route path="login" element={<div>Login</div>} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <App />
   </StrictMode>
 );
