3ea5b0312ee3b8706ff67ddbd73a6908dde8f702
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-09-06 09:01:02 +0200

    Separated reusable components

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <header>
+      <a href="" className="logo">
+        MyBlogs
+      </a>
+      <nav>
+        <a href="">Login</a>
+        <a href="">Register</a>
+      </nav>
+    </header>
+  );
+}
---
+++ client/src/components/Post.jsx
@@ -0,0 +1,30 @@
+export default function Post() {
+  return (
+    <div className="post">
+      <div className="image">
+        <img
+          src="https://techcrunch.com/wp-content/uploads/2024/08/GettyImages-489467998.jpg?resize=1536,1022"
+          alt="Blog Image"
+        />
+      </div>
+
+      <div className="texts">
+        <h2>
+          Telegram quietly enables users to report private chats to moderators
+          after founder arrest
+        </h2>
+        <p className="info">
+          <a href="" className="author">
+            Victor Mugisha
+          </a>
+          <time>2024-09-06 12:32</time>
+        </p>
+        <p className="summary">
+          The messaging app, which serves nearly 1 billion monthly active users,
+          has long maintained a reputation for minimal supervision of user
+          interactions.
+        </p>
+      </div>
+    </div>
+  );
+}
